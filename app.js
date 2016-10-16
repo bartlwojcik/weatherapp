8110beaf27dcd5b02ef3b416872d6e8eac27a370
Bartłomiej Wójcik <bartlomiejwojcik92@gmail.com>
2016-10-16 18:51:39 +0200

    making input work

--- app.js
+++ app.js
@@ -5,29 +5,39 @@ $(function () {
     var icon = $('#icon');
     var weather = $('#weather');
     var apiKey = '277b74aa77fdfe56ca1ebdf913639f93';
-    
+
+    $(window).on('keydown', function(e) {
+      if (e.keyCode == 13) {
+        e.preventDefault();
+        return false;
+      }
+    });
+
     $('#btn-geoloc').on('click', function () {
-        
-        name.html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+
+        $('#citySearch').val('');
+        name.html('<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>');
         t.html('');
         icon.html('');
         weather.html('');
-        
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
             coordinates.html('your browser doesn\t support geolocation');
         }
-        
+
         function showPosition(position) {
             /*coordinates.html('latitude: ' + position.coords.latitude + '<br>longitude: ' + position.coords.longitude);*/
-            
+
             $.ajax({
                 type: "GET",
                 url: "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid=" + apiKey,
                 success: function (data) {
                     name.html(data.name);
-                    t.html(data.main.temp + '&deg;C');
+                    var temp = data.main.temp;
+                    var tempRound = temp.toFixed(0);
+                    t.html(tempRound + '&deg;C');
                     icon.html("<img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\" width='90px'>");
                     weather.html(data.weather[0].main);
                 },
@@ -35,25 +45,29 @@ $(function () {
             });
         }
     });
-    
-    $('#btn-city').on('click', function () {
-        
+
+    $('#btn-city').on('click keydown', function () {
+
         var search = $('#citySearch').val();
-        name.html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+        name.html('<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>');
         t.html('');
         icon.html('');
         weather.html('');
-        
+
         $.ajax({
             type: "GET",
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=metric&appid=" + apiKey,
             success: function (data) {
                 name.html(data.name);
-                t.html(data.main.temp + '&deg;C');
+                var temp = data.main.temp;
+                var tempRound = temp.toFixed(0);
+                t.html(tempRound + '&deg;C');
                 icon.html("<img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\" width='90px'>");
                 weather.html(data.weather[0].main);
             },
             dataType: 'jsonp'
         });
+
+        $('#citySearch').val('');
     });
 });
